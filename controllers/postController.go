95eb542b51147edf5706fb76c43ef311272f8b97
Md Saiful Islam <saiful.rana@gmail.com>
2025-05-10 11:50:14 +0600

    curd done

--- controllers/postController.go
+++ controllers/postController.go
@@ -7,7 +7,7 @@ import (
 )
 
 func PostsCreate(c *gin.Context) {
-
+	// insert data
 	var body struct {
 		Title string
 		Body  string
@@ -26,4 +26,60 @@ func PostsCreate(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"post": post,
 	})
+}
+
+func PostsIndex(c *gin.Context) {
+	// get post data
+	var posts []models.Post
+	initializers.DB.Find(&posts)
+
+	c.JSON(200, gin.H{
+		"posts": posts,
+	})
+}
+
+func PostsShow(c *gin.Context) {
+	// get post by id
+
+	var post models.Post
+	id := c.Param("id")
+	initializers.DB.First(&post, id)
+
+	c.JSON(200, gin.H{
+		"post": post,
+	})
+}
+
+func PostsUpdate(c *gin.Context) {
+	// update post data
+	var body struct {
+		Title string
+		Body  string
+	}
+	c.Bind(&body)
+
+	var post models.Post
+	id := c.Param("id")
+	initializers.DB.First(&post, id)
+
+	post.Title = body.Title
+	post.Body = body.Body
+
+	initializers.DB.Model(&post).Updates(models.Post{
+		Title: post.Title,
+		Body:  post.Body,
+	})
+
+	c.JSON(200, gin.H{
+		"post": post,
+	})
+}
+
+func PostsDelete(c *gin.Context) {
+	// delete post data
+	var post models.Post
+	id := c.Param("id")
+	initializers.DB.Delete(&post, id)
+
+	c.Status(200)
 }
--- main.go
+++ main.go
@@ -15,6 +15,10 @@ func main() {
 
 	r := gin.Default()
 	r.POST("/posts", controllers.PostsCreate)
+	r.GET("/posts", controllers.PostsIndex)
+	r.GET("/posts/:id", controllers.PostsShow)
+	r.PUT("/posts/:id", controllers.PostsUpdate)
+	r.DELETE("/posts/:id", controllers.PostsDelete)
 
 	r.Run()
 }
